f15fdbd3bd63651fb1b834b76f648e694f03c534
Fobur <alexy1732@gmail.com>
2021-04-11 14:07:57 +0500

    Update 1

---
+++ RPG/Tile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG
+{
+    public class Tile
+    {
+        public readonly int XCord;
+        public readonly int YCord;
+        GameObject Content;
+
+        Tile(int x, int y, GameObject obj)
+        {
+            XCord = x;
+            YCord = y;
+            Content = obj;
+        }
+    }
+}
